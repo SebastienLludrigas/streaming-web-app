7d13e34c68f3a16edd5241de801869a7410e8863
Sebastien Lludrigas <sebastienlludrigas@gmail.com>
2020-09-06 21:36:16 +0200

    jsx header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './app.scss';
+import Header from '../Header/header';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './Icones/icone_twitch.svg';
+import search from './Icones/search.svg';
+import menuIco from './Icones/menu_ico.svg';
+
+const Header = () => {
+
+   return (
+      <div>
+         <nav className="headerTop">
+            <ul className="listeMenu">
+               <li className="liensNav">
+                  <img src={logo} alt="logo twitch" className="logo"/>
+               </li>
+               <li className="liensNav">
+                  Top Games
+               </li>
+               <li className="liensNav">
+                  Top streams
+               </li>
+               <li className="liensNav">
+                  <form className="formSubmit">
+                     <input type="text" className="inputRecherche"/>
+                     <button type="submit">
+                        <img src={search} alt="icone loupe" className="logoLoupe"/>
+                     </button>
+                  </form>
+               </li>
+            </ul>
+         </nav>
+         <div className="menuResBtn">
+            <img src={menuIco} alt="icone menu responsive" className="menuIco"/>
+         </div>
+      </div>
+   )
+}
+
+export default Header;
